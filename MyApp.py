8c4b21aa1e3fbe03810db2fd54e909ab3395f0c7
Vysse <etiennelecerf@gmail.com>
2024-05-25 18:46:20 +0200

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -290,19 +290,20 @@ def main():
     st.header('"The person that turns over the most rocks wins the game."')
     gumcode = """<script src="https://gumroad.com/js/gumroad.js"></script>
     <a class="gumroad-button" href="https://vysse.gumroad.com/l/ZeUmF" data-gumroad-overlay-checkout="true">Buy on</a>"""
-    #st.sidebar.link_button('I get my License Key','https://vysse.gumroad.com/l/ZeUmF')
+    
+    
     with st.sidebar:
         st.header('Settings')
-        components.html(gumcode,height=600)
+        st.link_button('I get my License Key','https://vysse.gumroad.com/l/ZeUmF')
     # License key check
     if 'license_valid' not in st.session_state:
-        license_key = st.sidebar.text_input("Enter your license key", type="password",autocomplete="license-key")
+        license_key = st.text_input("Enter your license key", type="password",autocomplete="license-key")
         if st.button('Validate License'):
             if check_license(license_key):
                 st.session_state['license_valid'] = True
                 st.rerun()
             else:
-                st.sidebar.error('Invalid License Key')
+                st.error('Invalid License Key')
 		    
     if st.session_state.get('license_valid', False):
         with st.form("Search"):
@@ -311,7 +312,7 @@ def main():
           display_graph()
         with st.spinner("load dataframe"):
           display_screener()
-
+    components.html(gumcode,height=600)
 # Run the app
 if __name__ == "__main__":
     main()
