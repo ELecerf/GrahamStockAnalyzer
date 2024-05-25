d64d370b01bfc1e8ccf5967227084e114c97e955
Vysse <etiennelecerf@gmail.com>
2024-05-25 18:00:50 +0200

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -290,10 +290,10 @@ def main():
     st.header('"The person that turns over the most rocks wins the game."')
     gumcode = """<script src="https://gumroad.com/js/gumroad.js"></script>
     <a class="gumroad-button" href="https://vysse.gumroad.com/l/ZeUmF">I get my key with</a>"""
-    
-    st.sidebar.header('Settings')
-    st.sidebar.components.html(gumcode)
-    st.sidebar.link_button('I get my License Key','https://vysse.gumroad.com/l/ZeUmF')
+    with st.sidebar:
+        st.header('Settings')
+        components.html(gumcode)
+        #st.sidebar.link_button('I get my License Key','https://vysse.gumroad.com/l/ZeUmF')
     # License key check
     if 'license_valid' not in st.session_state:
         license_key = st.sidebar.text_input("Enter your license key", type="password",autocomplete="license-key")
