377e5a4029bbe76ca9c4cf2893b578d670df775f
Vysse <etiennelecerf@gmail.com>
2024-05-27 11:24:13 +0200

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import requests
 from pymongo import MongoClient
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, today
 from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource
 import os
