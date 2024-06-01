d14c47da8f2b9474ff7bf2ba2a512ecf989472dd
Vysse <etiennelecerf@gmail.com>
2024-06-01 20:59:03 +0200

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -314,6 +314,7 @@ def main():
     <style>
     .stApp {
         background-image: linear-gradient(to right top, #1f4b4c, #2a7868, #57a673, #9ed273, #f9f871);
+        background-size: cover;
     }
     </style>
     """
