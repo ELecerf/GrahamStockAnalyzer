8050a21bcdbe38121e0139be285f4c8ca09f5670
Vysse <etiennelecerf@gmail.com>
2024-06-01 21:20:03 +0200

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -323,7 +323,7 @@ def main():
         box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
     }
     [data-testid="collapsedControl"] {
-    background-color: white !important
+    background-color: white !important;
     padding: 20px;
     border-radius: 10px;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
