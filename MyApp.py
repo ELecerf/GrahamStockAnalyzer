f82d1eac6685859666dd519aa4a4d418fa20c475
Vysse <etiennelecerf@gmail.com>
2024-06-01 21:17:42 +0200

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -324,6 +324,9 @@ def main():
     }
     [data-testid="collapsedControl"] {
     background-color: white !important
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
     }
     .stForm > div {
         background-color: white !important;
