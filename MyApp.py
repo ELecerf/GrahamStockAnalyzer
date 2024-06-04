b5c849824d9829e9e6724473c7d840d7122b44e5
Vysse <etiennelecerf@gmail.com>
2024-06-04 22:45:06 +0200

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -170,7 +170,7 @@ def search_stocks(query):
     else:
         return pd.DataFrame()  # Return an empty DataFrame if the query is empty
 
-def search_command():
+ddef search_command():
     st.title("Stock Search Tool")
 
     # Form for user input and search
@@ -183,7 +183,7 @@ def search_command():
             result_df = search_stocks(query)
             if not result_df.empty:
                 st.write("Search Results:")
-
+                
                 # Display the dataframe with selectable rows
                 selected_rows = st.dataframe(
                     result_df,
@@ -194,19 +194,22 @@ def search_command():
                 )
 
                 # Store the selected rows in the session state for further processing
+                st.session_state['result_df'] = result_df
                 st.session_state['selected_rows'] = selected_rows
-
-                # Form for plotting the selected row
-                with st.form("Plot Form"):
-                    plot_button = st.form_submit_button("Plot selection")
-                    if plot_button:
-                        process_selection(result_df)
             else:
                 st.info("No results found for your search.")
     elif search_button:
         st.info("Please enter a query to search for stocks.")
+    
+    # Check if there is selected data in session state
+    if 'selected_rows' in st.session_state:
+        with st.form("Plot Form"):
+            plot_button = st.form_submit_button("Plot selection")
+            if plot_button:
+                process_selection()
 
-def process_selection(result_df):
+def process_selection():
+    result_df = st.session_state.get('result_df')
     selected_rows = st.session_state.get('selected_rows')
     if selected_rows and 'selected_rows' in selected_rows:
         if selected_rows['selected_rows']:  # Check if any row is actually selected
