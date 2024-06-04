7ca70924f2ad95c0990adec16c218a26f7c8812d
Vysse <etiennelecerf@gmail.com>
2024-06-04 22:59:40 +0200

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -190,6 +190,7 @@ def search_command():
                     use_container_width=False,
                     hide_index=False,
                     selection_mode='single-row',
+                    on_select='rerun',
                     key='dataframe'
                 )
 
