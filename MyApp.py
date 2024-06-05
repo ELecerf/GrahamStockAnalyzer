56405a7e5a8b3f2818c0f507fd317747d8e85fd1
Vysse <etiennelecerf@gmail.com>
2024-06-05 13:04:11 +0200

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -403,7 +403,7 @@ def main():
     </style>
     """
     st.markdown(hide_default_format, unsafe_allow_html=True)
-    st.title('ValeurGraph')
+    st.title('ValeurGraph. Simple.')
     st.header('The simplest Deep Value app')
     st.markdown('**"The person that turns over the most rocks wins the game."**')
     st.divider()
