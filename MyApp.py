0a4c060f491e3cabfa1f0315776015155be01aa2
Vysse <etiennelecerf@gmail.com>
2024-06-14 21:16:37 +0200

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -112,7 +112,7 @@ def get_earnings(tick):
 
 def get_bsy_data(tick):
     columnOfInterest = ['commonStockSharesOutstanding','totalAssets','totalLiab','totalCurrentAssets',
-                        'netTangibleAssets','cash','totalStockholderEquity','totalCurrentLiabilities','Current Assets']
+                        'netTangibleAssets','cash','totalStockholderEquity','totalCurrentLiabilities']
     url = "https://eodhistoricaldata.com/api/fundamentals/%s"%tick
     params = {'api_token': EOD_API_KEY, 'filter': "Financials::Balance_Sheet",'fmt':'json'}
     r = requests.get(url, params=params)
