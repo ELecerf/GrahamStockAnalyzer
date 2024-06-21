2b9f2a01ad55e2cc8854d2aa997fc32eca1081cd
Vysse <etiennelecerf@gmail.com>
2024-06-21 21:01:01 +0200

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -310,13 +310,7 @@ def create_bokeh_chart(stock,df_fundamentals, df_stock):
     # Add glyphs
     p.line('date', 'adjusted_close', source=s2, legend_label='Close price', color='midnightblue', alpha=0.8)
     p.line('date', 'BookValuePerShare', source=s1, legend_label='Book Value per share', color='red', line_width=1)
-    '''
-    p.scatter('date', 'NCAV', source=s1, legend_label='Net Current Asset Value per share', color='blue', size=10, marker='y')
-    p.scatter('date', 'Graham_Number', source=s1, legend_label='Graham Number', color='green', marker='circle')
-    p.scatter('date', 'NTAV', source=s1, legend_label='Net Tangible Asset Value per share', color='black',marker='circle')
-    p.scatter('date', 'Cash', source=s1, legend_label='Cash', color='black',  size=10,marker='x')
-    p.scatter('date', '10EPS', source=s1, legend_label='10*EPS', color='orange', size=10,marker='triangle')
-    '''
+
     p.toolbar.logo = None
     p.axis.minor_tick_in = -3
     p.legend.location = "top_left"
