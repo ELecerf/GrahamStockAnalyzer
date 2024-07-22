503ecabbb173bbb1beba4e90f4ffd0e4ff4ded0c
Vysse <etiennelecerf@gmail.com>
2024-07-22 22:47:05 +0200

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -186,13 +186,11 @@ def get_bsy_data(tick):
     dfq=pd.DataFrame.from_dict(json['quarterly'],orient="index")[columnOfInterest]
     dfy.index=pd.to_datetime(dfy.index)
     dfq.index=pd.to_datetime(dfq.index)
-    # Combine data, prioritize the latest quarters
-    combined_df = pd.concat([dfq, dfy[~dfy.index.isin(dfq.index)]])
-    
-    # Sort and truncate to the latest 14 entries
-    combined_df = combined_df.sort_index(ascending=False)
-    combined_df.index.names = ['date']
-    return combined_df[:14]
+    df = pd.concat([dfq[:3],dfy])
+    df.index=pd.to_datetime(df.index)
+    df=df.sort_index(ascending=False)
+    df.index.names=['date']
+    return df[:14]
 
 
 def get_fundamentals(tick):
