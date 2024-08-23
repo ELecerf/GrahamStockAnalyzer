b108dc0df227f297f4e1852ab65b9df298c9b30a
Vysse <etiennelecerf@gmail.com>
2024-08-23 12:37:13 +0200

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -306,7 +306,7 @@ def display_screener():
 
     # List of exchanges
     exchanges = sorted(['PA', 'XETRA','TSE', 'TO', 'MI', 'MC', 'US', 'AS', 'BR','WAR',
-               'OL','CO','ST','KLSE', 'HK'])
+               'OL','CO','ST','KLSE', 'HKG'])
     
     # Form for selecting an exchange and loading data
     with st.form("Exchange Selector"):
