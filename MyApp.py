126799e6b4351badae68edce17966b84ddfcba0a
Vysse <etiennelecerf@gmail.com>
2025-01-12 21:56:34 +0100

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -57,7 +57,7 @@ def load_data(exchanges=['TSE']):
         df = pd.DataFrame(list(cursor))
         columns = ['Name', 'Exchange', 'Code', 'close', 'GrahamNumberToPrice', 
 		   'NCAV_0toMarketCap', 'Net_Cash_to_MarketCap','NCAV_1toMarketCap',
-		   'Enterprising_Number','Crieria_Def2A','Country']
+		   'Enterprising_Number','Criteria_Def2A','DilutedEpsTTM','Country']
         return df[columns]
 @st.cache_data
 def load_data_NCAV():
